a79f9677dcc839a92cab9d753b530f22c7f90a24
jake123321122-wq <jake123321122@gmail.com>
2026-02-10 20:22:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'تحديث أمان جديد!';
+    const data = event.data ? event.data.text() : 'تنبيه أمني جديد!';
+    const options = {
+        body: data,
+        icon: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+        badge: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+        vibrate: [200, 100, 200]
+    };
+
     event.waitUntil(
-        self.registration.showNotification('Google Security', {
-            body: data,
-            icon: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_Ads_logo.svg'
-        })
+        self.registration.showNotification('Google Security', options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('https://google.com')
     );
 });
