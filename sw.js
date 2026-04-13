0012e7290c6349da454e3a9837c248c17638c864
jake123321122-wq <jake123321122@gmail.com>
2026-04-13 22:24:25 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data ? event.data.text() : 'تنبيه أمني من Google',
-        icon: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
-        vibrate: [100, 50, 100]
-    };
-    event.waitUntil(self.registration.showNotification('Security Alert', options));
-});
